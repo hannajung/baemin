a4645830ceb79a1645dce68f661c709d4ecec3af
Hanna <hanna4171@gmail.com>
2021-05-07 19:39:32 +0900

    Update a slide to responsive slide
    Change the size of the window of slide when the whole window size is changing

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 //toggleBtn이라는 변수에 클래스가 navbar_toggleBtn이라는 애를 연결.
-const toggleBtn = document.querySelector('.navbar1_toggleBtn');
-const items = document.querySelector('.navbar1_items');
-var image = document.getElementsById("image");
-var images = ["strawberryHolic.jpg","goldenPremier.jpg","ranking.jpg","NCSI.jpg"];
-var imgIndex = 0;
+// const toggleBtn = document.querySelector('.navbar1_toggleBtn');
+// const items = document.querySelector('.navbar1_items');
+// var image = document.getElementsById("image");
+// var images = ["strawberryHolic.jpg","goldenPremier.jpg","ranking.jpg","NCSI.jpg"];
+// var imgIndex = 0;
 
-toggleBtn.addEventListener('click', ()=>{
-    items.classList.toggle('active');
-});
+// toggleBtn.addEventListener('click', ()=>{
+//     items.classList.toggle('active');
+// });
 
-function changeImage(){
-    image.setAttribute("src", images[imgIndex]);
-    imgIndex ++;
-    if (imgIndex >= images.length){
-        imgIndex = 0;
-    }
-}
+// function changeImage(){
+//     image.setAttribute("src", images[imgIndex]);
+//     imgIndex ++;
+//     if (imgIndex >= images.length){
+//         imgIndex = 0;
+//     }
+// }
 
-document.getElementById("image").src = "strawberryHolic.jpg"
+// document.getElementById("image").src = "strawberryHolic.jpg"
 
-setInterval(changeImage, 1000);
+// setInterval(changeImage, 1000);
 
